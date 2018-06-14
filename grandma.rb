89fbc5f741ba9df2ed84e11e8160d2ec0ba427c6
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-14 17:20:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,10 @@
 # Write a speak_to_grandma method.
 input_phrase = "Hi Nana, how are you?"
 def speak_to_grandma(phrase)
-  # Check if it is false that string is all upper case
   if (phrase == phrase.upcase) == false
 
-    # return this string if condition is false
      puts "HUH?! SPEAK UP, SONNY!"
       else
-    # return this string if condition is true
       puts "NO, NOT SINCE 1938!"
   end
 end
