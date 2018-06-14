e72df41f80f61be6c301495224396d53e6d1cd1e
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-14 17:33:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,8 @@
 # Write a speak_to_grandma method.
 phrase = "Hi Nana, how are you?"
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase = "Hi Nana, how are you?")
   if (phrase != phrase.upcase)
-
-     puts "HUH?! SPEAK UP, SONNY!"
+      puts "HUH?! SPEAK UP, SONNY!"
       else
       puts "NO, NOT SINCE 1938!"
   end
